e70613b1f46e936460d7bf17a2098a5cab1623d7
NITESHBHARDWAJ001 <169159672+NITESHBHARDWAJ001@users.noreply.github.com>
2025-04-03 18:18:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import datetime as dt
 
 
 app = Flask(__name__, instance_path='/tmp')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/provider.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/provider.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = '17ce2afd1bf4a1e4eecbfbdf'
 login_manager = LoginManager()
